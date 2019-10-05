e0b031b2f51b9cef076141ad3ffd23eeae9ee29a
Melrok <martin.blascak86@gmail.com>
2019-10-05 11:50:14 +0200

    modifikacia projektu 17B - Melrok

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h2>Projekt17B</h2>
     <h1>Nový začiatok</h1>
     <h3>Este jeden mali podnadpis pre commit</h3>
   </div>
@@ -17,16 +18,28 @@ export default {
 
 <style>
 #app {
+  border: 2px solid #cecece;
+  border-radius: 50px 50px 10px 10px;
+  height: 80vh;
+  width: 80%;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #e4e4e4;
+  margin: 50px auto;
 }
 
 h1, h2 {
-  font-weight: normal;
+  font-weight: 900;
+}
+
+h2 {
+  color: red;
+  font-weight: 900;
+  text-decoration: underline;
+  font-style: italic;
 }
 
 ul {
